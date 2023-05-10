c37873b95f753438dc4c483d8f1582244384d9f1
ArtyomKhodas <a.Khodas1601@gmail.com>
2023-05-10 18:32:24 +0300

    completeHomeWork28

--- src/test/java/by/teachMeSkills/khodasArtyom/homework28/StringUtilTest.java
+++ src/test/java/by/teachMeSkills/khodasArtyom/homework28/StringUtilTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StringUtilsTest {
+class StringUtilTest {
 
     private static final char[] elements = {'а', 'р', 'т', 'е', 'м'};
 
